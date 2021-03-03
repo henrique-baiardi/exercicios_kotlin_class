8aa5e6693cd68ca06f41b7bad339fc729acf8a26
Henrique Mendonça <Henridex94@gmail.com>
2021-03-03 13:20:47 -0300

    exer 3 versao 1,00.00

--- src/main/kotlin/Exercicio03/Main.kt
+++ src/main/kotlin/Exercicio03/Main.kt
@@ -1,64 +1,111 @@
 package Exercicio03
 
-//O ideal eh ter mais um classe pai funcionarios
-class Funcionarios(var nome:String? ="",
-                   var id:String? ="",
-                   var cpf:String? ="",
-                   var salario:Double? = 0.0,
-                   var tipoFuncionario:String ="")
-
-//class Programador(var nome:String? ="", var id:String? ="",var cpf:String? ="", var salario:Double? = 0.0)
-//class Gestor(var nome:String? ="", var id:String? ="",var cpf:String? ="", var salario:Double? = 0.0)
-
+abstract class Funcionario(
+    var id: String? = "",
+    var name: String? = "",
+    var cpf: String? = "",
+    var salario: Double? = 0.0,
+    var cargo: String? = ""
+)
+//
+class Gestor() : Funcionario(),calcSalario {
+    override fun aumentarSalario(salario: Double?): Double? {
+        return salario?.times(1.1)
+    }
+}
+class Programador() : Funcionario(),calcSalario {
+    override fun aumentarSalario(salario: Double?): Double? {
+        return salario?.times(1.2)
+    }
+}
+//
+interface calcSalario{
+    fun aumentarSalario(salario:Double?):Double?
+}
+//
 fun main() {
 
     //main ADD
-    println("Digite o nome do funcionário:")
-    var nome = readLine()
     println("Digite o id do funcionário:")
     var id = readLine()
-    println("Digite o CPF do funcionário:")
+    println("Digite o nome do funcionário:")
+    var nome = readLine()
+    println("Digite o cpf do funcionário:")
     var cpf = readLine()
     println("Digite o Salario atual do funcionário:")
     var salario = readLine()?.toDouble()
     println("Digite seu cargo: (1)gestor ou (2)programador ")
     var cargo = readLine()
 
-//    var listaProgramadores:MutableList<Programador> = mutableListOf()
-//    var listaGestores:MutableList<Gestor> = mutableListOf()
-    var listaFuncionarios:MutableList<Funcionarios> = mutableListOf()
 
-    if (cargo != "1" && cargo != "2"){
+    var listaFuncionarios: MutableList<Funcionario> = mutableListOf()
+
+    if (cargo != "1" && cargo != "2") {
         println("Valor invalido")
-    }else {
-        var funcionarios = Funcionarios(nome,id,cpf,salario,cargo)
+    } else if (cargo == "1") {
+        var gestor = Gestor()
+        gestor.id = id
+        gestor.name = nome
+        gestor.cpf = cpf
+        gestor.salario = salario
+        gestor.cargo = cargo
 
-        listaFuncionarios.add(funcionarios)
+        listaFuncionarios.add(gestor)
+
+    } else if (cargo == "2") {
+        var programador = Gestor()
+        programador.id = id
+        programador.name = nome
+        programador.cpf = cpf
+        programador.salario = salario
+        programador.cargo = cargo
+
+        listaFuncionarios.add(programador)
     }
-
     //FIM main ADD
 
     //Main pesquisar
 
-    println("Digite o ID do funcionario:")
+    println("Digite o id do funcionario:")
     var selecionado = readLine()
+    var solucao: Any
 
-    var pesquisa = listaFuncionarios.filter { it.id == selecionado}
+    var pesquisa = listaFuncionarios.filter { it.id == selecionado }
 
-    while (pesquisa.count() == 0){
+    while (pesquisa.count() == 0) {
         println("Id nao encontrado...")
-        println("Digite o ID do funcionario:")
+        println("Digite o id do funcionario:")
         selecionado = readLine()
 
-        pesquisa = listaFuncionarios.filter { it.id == selecionado}
+        pesquisa = listaFuncionarios.filter { it.id == selecionado }
     }
+    println("ID encontrado com sucesso!")
+
     println("1- Exibir seus Dados | 2 - Aumentar Salario")
-    var resposta = readLine()
-//    if ()
-//    for (func in pesquisa) {
-//        println("${func.nome}, ${func.id}, ${func.cpf}, ${func.tipoFuncionario}, ${func.salario}")
-//    }
+    var opcao = readLine()
 
+    when (opcao) {
+        "1" -> {
+            for (func in pesquisa) {
+                println("ID: ${pesquisa[0].id}, Nome: ${pesquisa[0].name}, CPF: ${pesquisa[0].cpf}, R$${pesquisa[0].salario}, Cargo: ${pesquisa[0].cargo}")
+                }
+            }
+        "2" -> {
+            for (cont in listaFuncionarios){
+                if (pesquisa[0].id == cont.id){
+                   if (cont.cargo == "1"){
+                       cont.salario?.times(1.1)
+                       println(cont.salario)
+                   }else{
+                       cont.salario?.times(1.2)
+                       println(cont.salario)
+                   }
+                }
+            }
+        }
+        else -> {
+            println("Opcao Invalida")
+        }
 
-
+    }
 }
